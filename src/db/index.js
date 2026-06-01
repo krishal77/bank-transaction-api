3d0f3dadad62a814cc368f787b5cf2119cbaf913
krishal77 <kthapaliya77@gmail.com>
2026-06-01 18:51:41 +0545

    add cookie-parser and cors middleware, implement async error handler, and set up database connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../contants.js";
+import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
 
 const connectDB= async ()=>{
 try{
