2de4849fda8b47651ebfe224b53e4655645d4750
krishal77 <kthapaliya77@gmail.com>
2026-06-05 18:54:12 +0545

    ledger model final

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -42,4 +42,6 @@ ledgerSchema.pre('deleteOne',preventLedgerModification);
 ledgerSchema.pre('remove',preventLedgerModification);
 ledgerSchema.pre('deleteMany',preventLedgerModification);
 ledgerSchema.pre('updateMany',preventLedgerModification);
+ledgerSchema.pre('findOneAndDelete',preventLedgerModification);
+ledgerSchema.pre('findOneAndReplace',preventLedgerModification);
 export const Ledger= mongoose.model("Ledger",ledgerSchema)
