7f528f83d4487f54828155fbdca505ddb27f8a58
krishal77 <kthapaliya77@gmail.com>
2026-06-05 18:36:45 +0545

    transaction model

---
+++ src/models/transaction.model.js
@@ -0,0 +1,37 @@
+import mongoose,{Schema} from "mongoose";
+
+const transactionModel= new Schema({
+    fromAccount:{
+        type:mongoose.Types.ObjectId,
+        ref:"account",
+        required:[true,"transaction must be associated with a from account"],
+        index:true
+    },
+    toAccount:{
+        type:mongoose.Types.ObjectId,
+        ref:"account",
+        required:[true,"Transaction must be associated with a to account"],
+        index:true
+    },
+    status:{
+        typr:String,
+        emun:{
+            values:["PENDING","COMPLETED","FAILED","REVERSED"],
+            message:"Status can be either PENDING,COMPLETED,FAILED or REVERSED",
+        },
+        default:"PENDING"
+    },
+    amount:{
+        type:Number,
+        required:[true,"Amount is required for creating a transaction"],
+       
+},
+ idempotencyKey:{
+    type:String,
+    required:[true,"Idempotency Key is required for creating a transaction"],
+    index:true,
+    unique:true
+ }
+},{timestamps:true})
+
+export const Transaction= mongoose.model("Transaction",transactionModel)
